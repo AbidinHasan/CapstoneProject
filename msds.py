9ba05cbd74db0bd31b5e8d7edb58915567fba714
bidin <muhammadabidinhasan@gmail.com>
2026-04-22 14:41:48 +0700

    first commit

---
+++ msds.py
@@ -0,0 +1,5 @@
+#cb8
+nutrisi = 'https://ik.imagekit.io/galleryBiden/dokumen/MSDS_ULTRA%20NUTRI%20BIO%20S.pdf?updatedAt=1751893181120'
+bakteriStater = 'https://ik.imagekit.io/galleryBiden/dokumen/MSDS_ULTRA%20BIOTIFIX.pdf?updatedAt=1751893181374'
+pac = 'https://ik.imagekit.io/galleryBiden/dokumen/MSDS_WTC-01.pdf?updatedAt=1751893180982'
+naoh = 'https://ik.imagekit.io/galleryBiden/dokumen/MSDS_CAUSTIC-SODA.pdf?updatedAt=1751898622026'
